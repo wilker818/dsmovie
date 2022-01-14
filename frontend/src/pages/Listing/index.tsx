b5f6afbc47dc7bcbdb395aed0ac82c31b0462aa4
wilker818 <wilkerferreira818@gmail.com>
2022-01-14 20:13:22 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,16 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 
 function Listing() {
+    // FORMA ERRADA TESTE DE REQUISIÇÃO
+    axios.get(`http://localhost:8080/movies?size=12&page=0`)
+        .then(response => {
+            console.log(response.data);
+        });
+    
+    
+    
     return (
         <>
             <Pagination />
