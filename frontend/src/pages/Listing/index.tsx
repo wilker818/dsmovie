173b081ccdbce21c00719d5de363865986a3676f
wilker818 <wilkerferreira818@gmail.com>
2022-01-14 20:40:56 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,18 +1,34 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useState, useEffect } from "react";
+import { MoviePage } from "types/movie";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
-    // FORMA ERRADA TESTE DE REQUISIÇÃO
-    axios.get(`http://localhost:8080/movies?size=12&page=0`)
+
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=0`)
         .then(response => {
-            console.log(response.data);
+            const data = response.data as MoviePage;
+            setPageNumber(data.number);
         });
+    }, []);
+
+    // FORMA ERRADA TESTE DE REQUISIÇÃO
+    //axios.get(`${BASE_URL}/movies?size=12&page=0`)
+    //    .then(response => {
+    //        const data = response.data as MoviePage;
+    //        setPageNumber(data.number);
+    //    });
     
     
     
     return (
         <>
+        <p>{pageNumber}</p>
             <Pagination />
             <div className="container">
                 <div className="row">
